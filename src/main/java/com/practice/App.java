f6cd66f324f8d6978627b87bf5bc8d2af68215db
manasranjan <manaskashmira@gmail.com>
2023-10-10 23:40:50 +0530

    added new comment

--- src/main/java/com/practice/App.java
+++ src/main/java/com/practice/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "For practicing purpose" );
+        System.out.println( "For practicing purpose only" );
     }
 }
